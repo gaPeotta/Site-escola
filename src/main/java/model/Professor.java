a5e2e19763b3583bedc58e1662f109aa17398d8a
LucasAlmeida2212 <lucas.almeidacosta09@gmail.com>
2026-02-03 18:57:31 -0300

    model Professor

---
+++ src/main/java/model/Professor.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Professor {
+    private Integer idProfessor;
+    private String nome;
+    private String disciplina;
+    private String senha;
+    private String usuario;
+
+    public void Professor(Integer idProfessor ,String nome,String disciplina,String senha,String usuario ){
+    this.idProfessor = idProfessor;
+    this.nome = nome;
+    this.disciplina = disciplina;
+    this.senha = senha;
+    this.usuario = usuario;
+    }
+
+    public Professor(String nome, String disciplina, String senha, String usuario) {
+        this.nome = nome;
+        this.disciplina = disciplina;
+        this.senha = senha;
+        this.usuario = usuario;
+    }
+
+    public Integer getIdProfessor() {
+        return idProfessor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+}
