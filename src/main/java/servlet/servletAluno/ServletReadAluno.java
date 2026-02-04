c8112c7408e8f20958a39cf02b1f7cc5849d8cd8
LucasAlmeida2212 <lucas.almeidacosta09@gmail.com>
2026-02-04 20:02:04 -0300

    criação servlet aluno

---
+++ src/main/java/servlet/servletAluno/ServletReadAluno.java
@@ -0,0 +1,17 @@
+package servlet.servletAluno;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ServletReadAluno {
+    @WebServlet(name = "ServletReadAluno", value = "/ServletReadAluno")
+    public class ServletReadProfessor {
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        }
+}
+}
